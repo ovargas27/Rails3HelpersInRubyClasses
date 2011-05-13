45cc0e37fbbd56fa9133620acf5a3c656e780fbc
Omar <ovargas27@gmail.com>
2011-05-13 17:33:33 -0500

    Display errors

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,5 +8,10 @@ class ContactsController < ApplicationController
     @contact.name = params[:contact][:name]
     @contact.email = params[:contact][:email]
     @contact.comment = params[:contact][:comment]
+    if @contact.valid?
+      @contact.send_email
+    else
+      render :action => :show
+    end
   end
 end
