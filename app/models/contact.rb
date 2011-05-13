0b238413140331d3692b4553afcf49a164014b82
Omar <ovargas27@gmail.com>
2011-05-13 15:51:07 -0500

    Contact model is compatible with Rails 3 helpers

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,32 @@
 class Contact
+  extend ActiveModel::Naming
   include ActiveModel::Validations
 
   attr_accessor :name, :email, :comment
 
   validates :name, :email, :comment, :presence => true
 
+  def attributes=(attrs)
+    attrs.each_pair { |name, value| self.send("#{name}=", value) }
+  end
+
+  def persisted?
+    false
+  end
+
+  def new_record?
+    true
+  end
+
+  def to_model
+    self
+  end
+
+  def to_key
+    [object_id]
+  end
+
+  def to_param
+    nil
+  end
 end
