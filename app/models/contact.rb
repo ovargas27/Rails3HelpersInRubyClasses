1641cc754966356cf7567efca561941c136b4f92
Omar <ovargas27@gmail.com>
2011-05-14 13:36:34 -0500

    Using view-helpers gem

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,34 +1,9 @@
 class Contact
-  extend ActiveModel::Naming
-  include ActiveModel::Validations
+  include ViewsHelpers::Model
 
   attr_accessor :name, :email, :comment
 
   validates :name, :email, :comment, :presence => true
-
-  def attributes=(attrs)
-    attrs.each_pair { |name, value| self.send("#{name}=", value) }
-  end
-
-  def persisted?
-    false
-  end
-
-  def new_record?
-    true
-  end
-
-  def to_model
-    self
-  end
-
-  def to_key
-    [object_id]
-  end
-
-  def to_param
-    nil
-  end
 
   def send_email
     if self.valid?
